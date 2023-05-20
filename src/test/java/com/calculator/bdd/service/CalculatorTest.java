c3ba133a471e2bddae276d603b9ddc28fd2cebeb
uday8247 <uday90sabbisetty@gmail.com>
2023-05-20 20:37:39 +0530

    Added test for accepting multiple integers for addition

--- src/test/java/com/calculator/bdd/service/CalculatorTest.java
+++ src/test/java/com/calculator/bdd/service/CalculatorTest.java
@@ -1,33 +1,43 @@
 package com.calculator.bdd.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.calculator.bdd.model.Calculator;
 import com.calculator.bdd.model.CalculatorImpl;
 
-public class CalculatorTest {
+class CalculatorTest {
+	
+	private Calculator calculator;
+	
+	@BeforeEach
+	void run() {
+		calculator = new CalculatorImpl();
+	}
 	
 	@Test
 	void testSumWithEmptyString() {
-		Calculator calculator = new CalculatorImpl();
 		int sum = calculator.add("");
 		Assertions.assertEquals(0, sum);
 	}
 	
 	@Test
 	void testSumWithOneNumber() {
-		Calculator calculator = new CalculatorImpl();
 		int sum = calculator.add("1");
 		Assertions.assertEquals(1, sum);
 	}
 	
 	@Test
 	void testSumWithTwoNumbers() {
-		Calculator calculator = new CalculatorImpl();
 		int sum = calculator.add("1,2");
 		Assertions.assertEquals(3, sum);
 	}
 	
+	@Test
+	void testSumWithMoreThanTwoNumbers() {
+		int sum = calculator.add("1,2,3,4,5");
+		Assertions.assertEquals(15, sum);
+	}
 	
 }
