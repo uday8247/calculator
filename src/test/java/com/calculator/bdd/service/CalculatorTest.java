7bb93973bc7b68f1b300ea86a3b3b5697ff35d04
uday8247 <uday90sabbisetty@gmail.com>
2023-05-20 20:17:10 +0530

    Added test case for single number

--- src/test/java/com/calculator/bdd/service/CalculatorTest.java
+++ src/test/java/com/calculator/bdd/service/CalculatorTest.java
@@ -6,13 +6,20 @@ import org.junit.jupiter.api.Test;
 import com.calculator.bdd.model.Calculator;
 import com.calculator.bdd.model.CalculatorImpl;
 
-public class Some {
+public class CalculatorTest {
 	
 	@Test
 	void testSumWithEmptyString() {
 		Calculator calculator = new CalculatorImpl();
 		int sum = calculator.add("");
 		Assertions.assertEquals(0, sum);
+	}
+	
+	@Test
+	void testSumWithOneString() {
+		Calculator calculator = new CalculatorImpl();
+		int sum = calculator.add("1");
+		Assertions.assertEquals(1, sum);
 	}
 	
 }
